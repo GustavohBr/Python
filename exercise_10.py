74a945a262181e69346c37ccc5fb6afdfe04cb2a
Gustavo Braz <GustavohBr@users.noreply.github.com>
2017-02-13 16:02:56 -0200

    Exercise 10

---
+++ exercise_10.py
@@ -0,0 +1,4 @@
+# Exercise 10
+
+print(list(set([i for i in [1,1,2,3,5,8,13,21,34,55,89] if i in [1,2,3,4,5,6,7,8,9,10,11,12,13]]))) 
+#The set() method converts the list to a set and removes the duplicates in said list
